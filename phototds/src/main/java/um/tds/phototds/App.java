4e51ceb402bdbef1c83a25f6ceaac105380f7821
RaulAlhama <raul.garciaa@um.es>
2022-11-14 16:28:38 +0100

    Imagenes, Lanzador y POM

--- phototds/src/main/java/um/tds/phototds/App.java
+++ phototds/src/main/java/um/tds/phototds/App.java
@@ -2,13 +2,15 @@ package um.tds.phototds;
 
 import java.awt.EventQueue;
 
+import um.tds.phototds.vistas.Login;
+
 public class App {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			
 			public void run() {
 				try {
-					Login2 ventanaLogin = new Login2();
+					Login ventanaLogin = new Login();
 					ventanaLogin.mostrarVentana();
 				} catch (Exception e) {
 					e.printStackTrace();
